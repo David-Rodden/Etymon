68563bf61b31572b3b13b1e53d4af35091b8611e
David-Rodden <davidrodden273@gmail.com>
2018-09-30 13:49:48 -0700

    Word can have multiple etymologies depending on their meaning

--- Etymon/website_reader.py
+++ Etymon/website_reader.py
@@ -3,10 +3,13 @@ from wiktionaryparser import WiktionaryParser
 
 class Website_Reader:
     def __init__(self, name: str):
-        self.etymological_sentence = WiktionaryParser().fetch(name)[0]["etymology"]
+        self.etymological_sentence = WiktionaryParser().fetch(name)
 
     def __repr__(self):
         return self.etymological_sentence
 
+    def get_first_etymology(self):
+        return self.etymological_sentence[0]["etymology"]
 
-print(Website_Reader("cool"))
+
+print(Website_Reader("first").get_first_etymology())
