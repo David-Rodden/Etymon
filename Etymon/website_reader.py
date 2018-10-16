49f6370a5d8effd09c457eacdc95829b9999c160
David-Rodden <davidrodden273@gmail.com>
2018-10-15 20:57:26 -0700

    Incorporating SequenceMatcher to recognize lexical similarity between specified word and those found in the etymological sentence

--- Etymon/website_reader.py
+++ Etymon/website_reader.py
@@ -1,15 +1,35 @@
+import re
+from difflib import SequenceMatcher
+
 from wiktionaryparser import WiktionaryParser
+
+
+class WordNode:
+    def __init__(self, word: str, child):
+        self.word = word
+        self.child = child
+
+    def set_child(self, word: str):
+        self.child = WordNode(word)
 
 
 class Website_Reader:
     def __init__(self, name: str):
+        self.name = name
         self.etymological_sentence = WiktionaryParser().fetch(name)
 
     def __repr__(self):
-        return self.etymological_sentence
+        return "this -> " + str(len(self.etymological_sentence))
 
-    def get_first_etymology(self):
-        return self.etymological_sentence[0]["etymology"]
+    def __get_first_type(self):
+        return self.etymological_sentence[0]["definitions"][0]["partOfSpeech"][0]
+
+    def __get_first_etymology(self):
+        return self.etymological_sentence[0]["etymology"] if self.etymological_sentence else None
+
+    def get_sorted_etymological_background(self):
+        return sorted(re.split("\s+", re.sub("([^\s\w]|_)+", "", self.__get_first_etymology())),
+                      key=lambda x: SequenceMatcher(None, self.name, x).ratio(), reverse=True)
 
 
-print(Website_Reader("first").get_first_etymology())
+print(Website_Reader("wolf").get_sorted_etymological_background())
